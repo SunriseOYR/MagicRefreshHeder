272db5eb8d880f6eb7f61279238d8836d660c8aa
OrangesAL <1263107985@qq.com>
2018-05-07 20:02:21 +0800

    init code

---
+++ MagicRefreshHeder/MagicRefreshHeder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MagicRefreshHeder
+//
+//  Created by 欧阳荣 on 2018/5/7.
+//  Copyright © 2018年 欧阳荣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
